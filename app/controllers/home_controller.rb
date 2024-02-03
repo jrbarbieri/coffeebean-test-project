fef77558e0c159942090925c2c027bbb60d8e999
Joao Barbieri <j.rafaeel@hotmail.com>
2024-02-03 19:56:31 -0300

    Cria funcao de logar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,27 @@
 class HomeController < ApplicationController
   def index
-    
+    return render :index if USERS.logged_user.empty?
+
+    redirect_to signin_path, params: { name: auth }
+  end
+
+  def authenticate
+    if auth.nil?
+      flash.now[:alert] = "Email or password are incorrect."
+      render :index, status: :unprocessable_entity
+    else
+      USERS.set_logged_user(user_params[:email])
+      redirect_to signin_path, params: { name: auth, email: user_params[:email] }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+
+  def auth
+    USERS.authenticate(user_params[:email], user_params[:password])
   end
 end
