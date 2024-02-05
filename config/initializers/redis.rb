ee2d8c8d2e4a6d4c39f49f5c69eaaffffd0ff095
Joao Barbieri <j.rafaeel@hotmail.com>
2024-02-05 15:53:24 -0300

    Corrige initializer do Redis para funcionar em prod e localmente

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 require 'redis'
 
 if ENV["REDISCLOUD_URL"]
-  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"] || 'redis://localhost:6379/1')
+  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+else
+  $redis = Redis.new
 end
